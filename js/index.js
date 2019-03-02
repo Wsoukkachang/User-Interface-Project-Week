205b1c6c90c0f2157509a727b9d633cac5bd5265
William Soukkachang <WSoukkachang@yahoo.com>
2019-03-02 13:28:35 -0600

    still working on nav expanded

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
 // JS goes here
 
+
+let menuButton = document.querySelector('.nav-bar img');
+
+//
+menuButton.addEventListener('click', () => {
+   menu.classList.toggle('nav-bar-active');
+   
+   // changes images of navButton 
+   menuButton.src.includes('img/nav-hamburger.png') ? menuButton.src = 'img/nav-hamburger-close.png' : menuButton.src = 'img/nav-hamburger.png';
+})
+
+
+let menu = document.querySelector('.nav-bar');
+
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open');
+}
+
 class TabLink {
     constructor(element) {
         this.element = element;
